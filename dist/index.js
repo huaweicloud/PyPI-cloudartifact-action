31b87d43e903d795c92985c96d1ea44d0c74370d
Chenyt26 <2396164919@qq.com>
2022-06-14 15:58:57 +0800

    build
    Match-id-8a62406a9d57dbcc350c5d523cc5694aa8198e03

--- dist/index.js
+++ dist/index.js
@@ -93,6 +93,7 @@ const context = __importStar(__nccwpck_require__(842));
 const utils = __importStar(__nccwpck_require__(918));
 const twine = __importStar(__nccwpck_require__(721));
 const pypi = __importStar(__nccwpck_require__(192));
+const pip = __importStar(__nccwpck_require__(121));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         core.info('Generate configurations for PyPI');
@@ -102,16 +103,102 @@ function run() {
             core.setFailed('parameter is not correct.');
             return;
         }
-        // 安装依赖工具twine
-        yield twine.installTwine();
-        // 生成.pypirc配置内容
-        pypi.generatePypirc(inputs);
+        if (inputs.pypiOperationType === 'upload') {
+            core.info('Generate pypirc configurations for uploading PyPI packages.');
+            // 安装依赖工具twine
+            yield twine.installTwine();
+            // 生成.pypirc配置内容
+            pypi.generatePypirc(inputs);
+            return;
+        }
+        if (inputs.pypiOperationType === 'install') {
+            core.info('Generate pip configurations for downloading PyPI packages.');
+            // 生成pip配置内容
+            pip.generatePipConfig(inputs);
+        }
         core.info(utils.getPypiTips(inputs));
     });
 }
 exports.run = run;
 run().catch(core.setFailed);
 //# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 121:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.generatePipConfig = exports.writePipConfig = exports.getPipPath = exports.getPipContents = void 0;
+const core = __importStar(__nccwpck_require__(186));
+const os = __importStar(__nccwpck_require__(37));
+const path = __importStar(__nccwpck_require__(17));
+const fs = __importStar(__nccwpck_require__(147));
+function getPipContents(inputs) {
+    const indexUrlContents = `index-url = ${inputs.indexUrl}`;
+    const trustedHostContents = `trusted-host = ${inputs.trustedHost}`;
+    const pipContents = `[global]\n${indexUrlContents}\n${trustedHostContents}`;
+    return pipContents;
+}
+exports.getPipContents = getPipContents;
+function getPipPath(platform) {
+    switch (platform.toLowerCase()) {
+        case 'linux':
+            return path.join(os.homedir(), '.pip', 'pip.conf');
+        case 'darwin':
+            return path.join(os.homedir(), '.pip', 'pip.conf');
+        case 'win32':
+            return path.join(os.homedir(), 'pip', 'pip.ini');
+        default:
+            throw new Error('The pip supports only Linux, Darwin and Windows platforms.');
+    }
+}
+exports.getPipPath = getPipPath;
+function writePipConfig(pipConfigPath, pipConfigContent) {
+    // 不存在(.pip/pip)目录即创建
+    if (!fs.existsSync(path.dirname(pipConfigPath))) {
+        core.info(`Pip Config Path:${pipConfigPath} does not exist.`);
+        fs.mkdirSync(path.dirname(pipConfigPath));
+    }
+    fs.writeFileSync(pipConfigPath, pipConfigContent);
+}
+exports.writePipConfig = writePipConfig;
+/**
+ * 根据用户输入生成pip install配置并写入对应路径
+ * @param inputs
+ */
+function generatePipConfig(inputs) {
+    const platform = os.platform();
+    core.info('The current platform is: ' + platform);
+    writePipConfig(getPipPath(platform), getPipContents(inputs));
+}
+exports.generatePipConfig = generatePipConfig;
+//# sourceMappingURL=pip.js.map
 
 /***/ }),
 
