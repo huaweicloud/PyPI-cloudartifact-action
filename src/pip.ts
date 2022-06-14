68ce997fccd04e0d3459e6d3f8716653358a1c2e
Chenyt26 <2396164919@qq.com>
2022-06-14 15:38:44 +0800

    pip installl config unit test
    Match-id-c6fc387b3ea6fdaeba6a78c770aada50e6e8a696

--- src/pip.ts
+++ src/pip.ts
@@ -2,15 +2,16 @@ import * as core from '@actions/core';
 import * as context from './context';
 import * as os from 'os';
 import * as path from 'path';
+import * as fs from 'fs';
 
 export function getPipContents(inputs: context.Inputs) {
-    const indexUrlContents = `index-url = ${inputs.indexUrl}\n`;
-    const trustedHostContents = `trusted-host = ${inputs.trustedHost}\n`;
-    const pipContents = `[global]\n[${indexUrlContents}]\n${trustedHostContents}`;
+    const indexUrlContents = `index-url = ${inputs.indexUrl}`;
+    const trustedHostContents = `trusted-host = ${inputs.trustedHost}`;
+    const pipContents = `[global]\n${indexUrlContents}\n${trustedHostContents}`;
     return pipContents;
 }
 
-function getPipPath(platform: string): string {
+export function getPipPath(platform: string): string {
     switch (platform.toLowerCase()) {
         case 'linux':
             return path.join(os.homedir(), '.pip', 'pip.conf');
@@ -25,5 +26,22 @@ function getPipPath(platform: string): string {
       }
   }
 
+export function writePipConfig(pipCobfigPath: string, pipConfigContent: string ) {
+    // 不存在(.pip/pip)目录即创建
+    if (!fs.existsSync(path.dirname(pipCobfigPath))) {
+      core.info(`Pip Config Path:${pipCobfigPath} does not exist.`);
+      fs.mkdirSync(path.dirname(pipCobfigPath));
+    }
+    fs.writeFileSync(pipCobfigPath, pipConfigContent);
+}
+
+/**
+ * 根据用户输入生成pip install配置并写入对应路径
+ * @param inputs
+ */
+export function generatePipConfig(inputs: context.Inputs) {
   const platform = os.platform();
-  core.info('platform: ' + getPipPath(platform));
+  core.info('The current platform is: ' + platform);
+  writePipConfig(getPipPath(platform), getPipContents(inputs));
+}
+  
---
+++ test/pip.test.ts
@@ -0,0 +1,122 @@
+import * as pip from '../src/pip';
+import fs from 'fs';
+import * as os from 'os';
+import {v4 as uuidv4} from 'uuid';
+
+test('test getPipContents', async () => {
+    const inputs = {
+        pypiOperationType: 'install',
+        indexUrl: 'https://pypi.org/simple',
+        trustedHost: 'pypi.org',
+        repository: '',
+        username: '',
+        password: '',
+        indexServer: ''
+    }
+    expect(pip.getPipContents(inputs)).toBe('[global]\nindex-url = https://pypi.org/simple\ntrusted-host = pypi.org');
+});
+
+
+describe('test normal getPipPath', () => {
+    const testCase = [
+        {platform: 'linux', result: 'pip.conf'},
+        {platform: 'darwin', result: 'pip.conf'},
+        {platform: 'win32', result: 'pip.ini'},
+    ];
+    testCase.forEach(item => {
+        const {platform, result} = item;
+        test(`platform输入为(${platform}),判断结果：${result}`, async () => {
+            expect(pip.getPipPath(platform)).toContain(result);
+        });
+    });
+});
+
+describe('test abnormal getPipPath', () => {
+    const testCase = [
+        {platform: 'freebsd', result: 'pip.conf'},
+        {platform: 'test', result: 'pip.conf'},
+    ];
+    testCase.forEach(item => {
+        const {platform, result} = item;
+        test(`platform输入为(${platform}),抛出异常`, async () => {
+            expect(() => pip.getPipPath(platform)).toThrow('The pip supports only Linux, Darwin and Windows platforms.');
+        });
+    });
+});
+
+describe('test generatePipConfig', () => {
+    const testCase = [
+        {
+            description: 'pypiOperationType!=install也!=upload',
+            inputs: {
+                pypiOperationType: 'install',
+                indexUrl: 'https://pypi.org/simple',
+                trustedHost: 'pypi.org',
+                repository: '',
+                username: '',
+                password: '',
+                indexServer: ''
+            },
+            result: '[global]\nindex-url = https://pypi.org/simple\ntrusted-host = pypi.org'
+        },
+    ];
+    const configPath = pip.getPipPath(os.platform());
+    const tmpPath = './test/pipini-' + uuidv4();
+    beforeAll(() => {
+        fs.copyFileSync(configPath, tmpPath);
+    });
+    afterAll(() => {
+        fs.copyFileSync(tmpPath, configPath);
+        fs.unlinkSync(tmpPath);
+    });
+
+    testCase.forEach(item => {
+        const {description, inputs, result} = item;
+        test(`${description}`, async () => {
+            pip.generatePipConfig(inputs);
+            expect(fs.readFileSync(configPath, 'utf-8')).toBe(result);
+        });
+    });
+});
+
+
+describe('test writePipConfig function', () => {
+    test('test write pipConfigContent into pipConfigPath', async () => {
+        const pipConfigContent = 'hello';
+        const pipConfigPath = 'test/.test-' + uuidv4();
+        pip.writePipConfig(pipConfigPath, pipConfigContent);
+        expect(fs.readFileSync(pipConfigPath, 'utf-8')).toBe(pipConfigContent);
+        fs.unlinkSync(pipConfigPath);
+    });
+    
+    test('test writePipConfig mock existsSync return true ', async () => {
+        jest.spyOn(fs, 'existsSync').mockReturnValue(true);
+        jest.spyOn(fs, 'mkdirSync').mockImplementation(() => '');
+        jest.spyOn(fs, 'writeFileSync').mockImplementation(() => '');
+        pip.writePipConfig('', '')
+        expect(fs.existsSync).toHaveBeenCalled();
+        expect(fs.existsSync).toHaveBeenCalledTimes(1);
+    
+        expect(fs.mkdirSync).not.toHaveBeenCalled();
+    
+        expect(fs.writeFileSync).toHaveBeenCalled();
+        expect(fs.writeFileSync).toHaveBeenCalledTimes(1);
+    
+    });
+    
+    test('test writePipConfig mock existsSync return false ', async () => {
+        jest.spyOn(fs, 'existsSync').mockReturnValue(false);
+        jest.spyOn(fs, 'mkdirSync').mockImplementation(() => '');
+        jest.spyOn(fs, 'writeFileSync').mockImplementation(() => '');
+        pip.writePipConfig('', '')
+        expect(fs.existsSync).toHaveBeenCalled();
+        expect(fs.existsSync).toHaveBeenCalledTimes(1);
+    
+        expect(fs.mkdirSync).toHaveBeenCalled();
+        expect(fs.mkdirSync).toHaveBeenCalledTimes(1);
+    
+        expect(fs.writeFileSync).toHaveBeenCalled();
+        expect(fs.writeFileSync).toHaveBeenCalledTimes(1);
+    
+    });
+});
